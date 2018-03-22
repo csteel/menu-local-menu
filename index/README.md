bd033b8cd8d9dbe616690ce4bedd3825435721b2
csteel <christopher.steel@mcgill.ca>
2018-03-22 09:40:57 -0400

    moved index dir into parent directory

---
+++ index/README.md
@@ -0,0 +1,10 @@
+# ACE Resources
+ 
+## Local Resource Index
+ 
+* [about](../help/about/index.html)
+ 
+## Additional Resources
+ 
+* [ACElab.ca](https://acelab.ca/)
+ 
--- menu-w.sh
+++ menu-w.sh
@@ -3,21 +3,28 @@
 # vars
 #
 MENU_DIRS_LIST_PATH=/tmp/directories.txt.tmp
-MENU_INDEX_README_PATH="$HOME/bin/menu/help/index/README.md"
+MENU_INDEX_README_PATH="$HOME/bin/menu/index/README.md"
+#
+# list directories
+#
 listdirs(){ set -- */; printf "%s\n" "${@%/}"; }
 cd "$HOME/bin/menu/help/"
 listdirs > $MENU_DIRS_LIST_PATH
 #
+# Create index README header
+#
 echo "# ACE Resources" > "$MENU_INDEX_README_PATH"
 echo " " >> "$MENU_INDEX_README_PATH"
-echo "## Local Resources" >> "$MENU_INDEX_README_PATH"
+echo "## Local Resource Index" >> "$MENU_INDEX_README_PATH"
 echo " " >> "$MENU_INDEX_README_PATH"
+#
+# Generate indexs list of help dir README.md files
 #
 MENUITEMS=""
 while IFS=$'
 ' read -r line || [[ -n "$line" ]]; do
 #   MENUITEMS="$MENUITEM * [$line]($line/$line.md)\n"
-   echo "* [$line](../$line/index.html)" >> "$MENU_INDEX_README_PATH"
+   echo "* [$line](../help/$line/index.html)" >> "$MENU_INDEX_README_PATH"
 #   echo $MENUITEMS
 done < "$MENU_DIRS_LIST_PATH"
 echo " " >> "$MENU_INDEX_README_PATH"
@@ -26,5 +33,5 @@ echo " " >> "$MENU_INDEX_README_PATH"
 echo "* [ACElab.ca](https://acelab.ca/)" >> "$MENU_INDEX_README_PATH"
 echo " " >> "$MENU_INDEX_README_PATH"
 cd $HOME/bin/menu
-firefox --new-window $HOME/bin/menu/help/index/index.html &&
+firefox --new-window $HOME/bin/menu/index/index.html &&
 exit
